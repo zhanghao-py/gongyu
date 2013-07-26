2b1edc1cb54e6a6401cdd4d962a08ff0cbf7a087
zhanghao-py <zhanghao_py@sina.com>
2013-07-26 09:10:16 -0700

    加入struts2

--- src/main/java/cm/h3c/college/pay/payment/web/action/HelloAction.java
+++ src/main/java/cm/h3c/college/pay/payment/web/action/HelloAction.java
@@ -1,5 +1,8 @@
 package cm.h3c.college.pay.payment.web.action;
 
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
@@ -11,17 +14,20 @@ import com.opensymphony.xwork2.ActionSupport;
 @Namespace("/hello")
 public class HelloAction extends ActionSupport {
 	
+	private Logger log = Logger.getLogger(HelloAction.class);
+	
 	private static final long serialVersionUID = 3921807869626262750L;
 
+	@Resource(name = "helloService")
 	private HelloService helloService;
 	
 	private Object data;
 	
-	@Action(value = "index", results = { @Result(name = "success", type = "json") })
+	@Action(value = "sayHello", results = { @Result(name = "success", type = "redirect", location = "/") })
 	public String sayHello() {
 		
 		String data = helloService.sayHello("zhanghao!");
-		
+		log.info(data);
 		
 		return SUCCESS;
 	}
