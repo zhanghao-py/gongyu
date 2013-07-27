3a106c3da93ce4d7c165f9264856132772543f61
zhanghao-py <zhanghao_py@sina.com>
2013-07-27 05:35:43 -0700

    添加sql 添加wsdl

---
+++ offline/db/main.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `gy_order` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `money` decimal NOT NULL COMMENT '角色名称',
+  `status` tinyint(4) NOT NULL COMMENT '1:订单初始化 2:支付中 3:支付完成',
+  `pay_result` tinyint(4) NOT NULL COMMENT '1:支付成功 2:支付失败',
+  `cams_result` tinyint(4) NOT NULL COMMENT '1:充值成功 2:充值失败',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `account` varchar(100) NOT NULL COMMENT '缴费账号',
+  `college_id` bigint(20) unsigned NOT NULL COMMENT '所属学校',
+  PRIMARY KEY (`id`),
+  KEY `order_index_college_id` (`college_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `gy_college` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT '学校名称',
+  `url` varchar(100) NOT NULL COMMENT 'CAMS URL',
+  `description` varchar(100) NOT NULL COMMENT '备注信息',
+  `operator_username` varchar(100) NOT NULL COMMENT '操作员-用户名',
+  `operator_password` varchar(100) NOT NULL COMMENT '操作员-密码',
+  `keystore_path` varchar(100) NOT NULL COMMENT '证书路径',
+  PRIMARY KEY (`id`),
+  KEY `college_index_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
