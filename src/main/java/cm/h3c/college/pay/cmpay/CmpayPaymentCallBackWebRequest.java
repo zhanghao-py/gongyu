488db841bb409a6a03c2821d215bfe91e8685028
majikun <ma.jikun@gmail.com>
2013-08-01 06:19:31 -0700

    toUppercase

--- src/main/java/cm/h3c/college/pay/cmpay/CmpayPaymentCallBackWebRequest.java
+++ src/main/java/cm/h3c/college/pay/cmpay/CmpayPaymentCallBackWebRequest.java
@@ -1,19 +1,19 @@
 package cm.h3c.college.pay.cmpay;
 
 public class CmpayPaymentCallBackWebRequest implements CmpaySignable {
-	String merId;// 商户编号 15位 M
-	String mid;// 系统跟踪号 14位 M
-	String omid;// 交易流水号 暂无意义 M
-	String txnTyp;// 业务类型 直接支付：DODIRECTPAYMENTB2C担保交易：B2C-DOGUARANTEE
+	String MERID;// 商户编号 15位 M
+	String MID;// 系统跟踪号 14位 M
+	String OMID;// 交易流水号 暂无意义 M
+	String TXNTYP;// 业务类型 直接支付：DODIRECTPAYMENTB2C担保交易：B2C-DOGUARANTEE
 					// C2C担保交易：C2C-DOGUARANTEE 预授权：DOAHTORIZATION
 					// 预授权确认：DOCAPTURE 预授权取消：DOVOID 订单查询：ORDERSEARCH 退款：
 					// ORDERREFUND M
-	String amout;// 支付金额 单位分 M
-	String orderId;// 原商户订单号 M
-	String orderDate;// 订单日期 M
-	String payDate;// 财务时间 M
-	String remark;// 备注 O
-	String status;// 支付结果
+	String AMOUT;// 支付金额 单位分 M
+	String ORDERID;// 原商户订单号 M
+	String ORDERDATE;// 订单日期 M
+	String PAYDATE;// 财务时间 M
+	String REMARK;// 备注 O
+	String STATUS;// 支付结果
 					// PADSUCCESS：预授权成功PADFAILED：预授权失败REFUSE：用户拒付SUCCESS：成功FAILED：失败
 					// M
 	String SIGN;// 签名 172位 M
--- src/main/java/cm/h3c/college/pay/cmpay/CmpayPaymentRequest.java
+++ src/main/java/cm/h3c/college/pay/cmpay/CmpayPaymentRequest.java
@@ -1,25 +1,25 @@
 package cm.h3c.college.pay.cmpay;
 
 public class CmpayPaymentRequest implements CmpaySignable {
-	String mcode = "101425";// 功能码 101425
-	String mid;// 系统跟踪号 14位
-	String date;// 交易日期 YYYYMMDD
-	String time;// 交易时间 HHMMSS
-	String merId;// 商户编号 15位
-	String orderId;// 商户订单号 20位
-	String amout;// 订单金额 最长15位，分为单位
-	String allowNote;// 是否允许用户对该笔订单进行评论 0表示允许，默认1表示不允许1位是否允许评论
-	String authorizeMode;// 推荐用户进行确认的方式 WEB
-	String currency;// 币种
+	String MCODE = "101425";// 功能码 101425
+	String MID;// 系统跟踪号 14位
+	String DATE;// 交易日期 YYYYMMDD
+	String TIME;// 交易时间 HHMMSS
+	String MERID;// 商户编号 15位
+	String ORDERID;// 商户订单号 20位
+	String AMOUT;// 订单金额 最长15位，分为单位
+	String ALLOWNOTE;// 是否允许用户对该笔订单进行评论 0表示允许，默认1表示不允许1位是否允许评论
+	String AUTHORIZEMODE;// 推荐用户进行确认的方式 WEB
+	String CURRENCY;// 币种
 					// CNY：可提现金额进行支付；用户只能选择可体现账户里的钱和银行账户里的钱进行支付。CMY：用不可提现金额支付，用户可以选择不可体现账户里面的钱+充值卡+银行卡进行支付3位
-	String orderDate;// 订单日期 YYYYMMDD
-	String period;// 有效期数量 3位
-	String periodUnit;// 有效期单位 1位1-月2-日3-小时4-分钟
-	String productDesc;// 产品描述 最长250位
-	String productId;// 产品编号 最长50位
-	String productName;// 产品名称 最长60位
-	String txntyp;// 交易类型 1位S：直接支付
-	String callBack;// 回调地址 商户的响应URL，用户在手机支付平台页面支付后，重定向和通知的地址最长100位
+	String ORDERDATE;// 订单日期 YYYYMMDD
+	String PERIOD;// 有效期数量 3位
+	String PERIODUNIT;// 有效期单位 1位1-月2-日3-小时4-分钟
+	String PRODUCTDESC;// 产品描述 最长250位
+	String PRODUCTID;// 产品编号 最长50位
+	String PRODUCTNAME;// 产品名称 最长60位
+	String TXNTYP;// 交易类型 1位S：直接支付
+	String CALLBACK;// 回调地址 商户的响应URL，用户在手机支付平台页面支付后，重定向和通知的地址最长100位
 	String MOBILEID;// 用户手机号 11位
 	String SIGN;// 签名 172位
 
